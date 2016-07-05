d9698672974d883f4b645f4a5f7448d6b0992694
Meaghan Jones and Maldon Meehan <maldon@>
2016-07-05 13:46:18 -0700

    add spec returns volume of the spec failing

---
+++ lib/parcel.rb
@@ -0,0 +1,6 @@
+class Parcel
+  define_method(:initialize) do |length, width, height, weight|
+
+
+  end
+end
--- spec/parcel_spec.rb
+++ spec/parcel_spec.rb
@@ -1,2 +1,11 @@
 require('rspec')
 require('parcel')
+
+describe(Parcel) do
+  describe("#volume") do
+    it("returns the volume of the box") do
+      test_parcel = Parcel.new(2,2,2,2)
+      expect(test_parcel.volume()).to(eq(8))
+    end
+  end
+end
