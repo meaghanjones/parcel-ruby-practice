1efc042f30f6ec8c127fb6abc930c427ffa3da3f
Meaghan Jones and Maldon Meehan <meaghan@>
2016-07-05 14:46:10 -0700

    returns the cost to ship spec test passing

--- lib/parcel.rb
+++ lib/parcel.rb
@@ -1,11 +1,40 @@
 class Parcel
-  define_method(:initialize) do |length, width, height|
+  define_method(:initialize) do |length, width, height, speed, weight, distance|
     @length = length
     @width = width
     @height = height
+    @speed = speed
+    @weight = weight
+    @distance = distance
   end
 
   define_method(:volume) do
-    @length*@width*@height
+    volume = @length*@width*@height
+    @volume = volume
+  end
+
+  define_method(:cost_to_ship) do
+    price = 0
+    if @speed == "regular"
+      price +=1
+    else
+      price +=2
+    end
+    if @weight == "light"
+      price +=1
+    else
+      price +=2
+    end
+    if @distance == "domestic"
+      price +=1
+    else
+      price +=2
+    end
+    if self.volume()<=10
+      price +=3
+    else
+      price +=5
+    end
+    price
   end
 end
--- spec/parcel_spec.rb
+++ spec/parcel_spec.rb
@@ -4,13 +4,14 @@ require('parcel')
 describe(Parcel) do
   describe("#volume") do
     it("returns the volume of the box") do
-      test_parcel = Parcel.new(2,2,2)
+      test_parcel = Parcel.new(2,2,2,"regular", "light", "domestic")
       expect(test_parcel.volume()).to(eq(8))
     end
   end
   describe('#cost_to_ship') do
     it("returns the cost to ship")do
       test_parcel = Parcel.new(2,2,2,"regular", "light", "domestic")
+      # test_parcel.volume()
       expect(test_parcel.cost_to_ship()).to(eq(6))
     end
   end
