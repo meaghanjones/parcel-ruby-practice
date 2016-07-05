263720dca2bae14b391ef586767848c45946c7ed
Maldon Meehan and Meaghan Jones <maldon+meaghan@>
2016-07-05 14:07:54 -0700

    returns the cost to ship failing spec

--- spec/parcel_spec.rb
+++ spec/parcel_spec.rb
@@ -8,4 +8,10 @@ describe(Parcel) do
       expect(test_parcel.volume()).to(eq(8))
     end
   end
+  describe('#cost_to_ship') do
+    it("returns the cost to ship")do
+      test_parcel = Parcel.new(2,2,2,"regular", "light", "domestic")
+      expect(test_parcel.cost_to_ship()).to(eq(6))
+    end
+  end
 end
